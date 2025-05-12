de5fb9d9fcf33bb11c229c066cbe8b1c3fa9b953
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2025-05-12 20:34:35 +0530

    adding mp4 players 	new file:   assets/ikatra.mp4 	new file:   assets/paro.mp4 	new file:   assets/playback.png 	new file:   assets/tere naina.mp4 	new file:   assets/tumse_milke_dil_ka.mp4 	renamed:    assets/song.mp4 -> assets/zara_zara.mp4 	modified:   index.html 	modified:   main.js 	deleted:    style.css 	new file:   style/style.css

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+            height: 630,
+            resizable: false,
+            frame: false,
+            transparent: true,
+            movable: true,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false
+            }
+        });
 
   win.loadFile('index.html')
 }
